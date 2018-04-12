63a47473f5fd78a977ed4f1b3a1e352addbcb0fd
steven le comber <s.c.lecomber@qmul.ac.uk>
2018-04-12 19:37:06 +0100

    Help file edits

--- R/RgeoProfile.R
+++ R/RgeoProfile.R
@@ -1,9 +1,6 @@
 #' RgeoProfile: Dirichlet Process Mixture (DPM) model of geographic profiling
 #'
-#' Carries out a DPM mixture model of geographic profiling (and associated functions) as described
-#' in Verity et al. (2014) and extended in Faulkner et al. (2016). For links to papers see 
-#' http:/www.sbcs.qmul.ac.uk/staff/stevenlecomber.html or contact Steven Le Comber at
-#' s.c.lecomber@qmul.ac.uk. Created and maintained by Bob Verity (r.verity@imperial.ac.uk).
+#' Carries out a DPM mixture model of geographic profiling (and associated functions) as described in Verity et al. (2014) For links to papers see http:/www.sbcs.qmul.ac.uk/staff/stevenlecomber.html or contact Steven Le Comber (s.c.lecomber@qmul.ac.uk). Created and maintained by Bob Verity (r.verity@imperial.ac.uk).
 #'
 #' @name RgeoProfile
 #' @docType package
