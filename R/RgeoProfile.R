abd34edd30d8c22ac7637eeb57727f74efc501d1
Steven Le Comber <s.c.lecomber@qmul.ac.uk>
2018-04-22 18:08:30 +0100

    Fixing corrupt .rdb
    Message saying RgeoProfile.rdb was corrupt so removing meaningless space to recommit

--- R/RgeoProfile.R
+++ R/RgeoProfile.R
@@ -1,6 +1,6 @@
 #' RgeoProfile: Dirichlet Process Mixture (DPM) model of geographic profiling
 #'
-#' Carries out a DPM mixture model of geographic profiling (and associated functions) as described in Verity et al. (2014) For links to papers see http:/www.sbcs.qmul.ac.uk/staff/stevenlecomber.html or contact Steven Le Comber (s.c.lecomber@qmul.ac.uk). Created and maintained by Bob Verity (r.verity@imperial.ac.uk).   
+#' Carries out a DPM mixture model of geographic profiling (and associated functions) as described in Verity et al. (2014) For links to papers see http:/www.sbcs.qmul.ac.uk/staff/stevenlecomber.html or contact Steven Le Comber (s.c.lecomber@qmul.ac.uk). Created and maintained by Bob Verity (r.verity@imperial.ac.uk).  
 #'
 #' @name RgeoProfile
 #' @docType package
