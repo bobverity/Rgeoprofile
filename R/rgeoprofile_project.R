305c0f6afd4646005a67718f34c03fbff4a2c502
bobverity <r.verity@imperial.ac.uk>
2018-10-03 10:53:16 +0100

    print project details

--- R/rgeoprofile_project.R
+++ R/rgeoprofile_project.R
@@ -79,7 +79,81 @@ print_full <- function(x, ...) {
 
 summary.rgeoprofile_project <- function(object, ...) {
   
-  message("TODO")
+  # print data summary
+  cat("DATA:\n")
+  if (is.null(object$data)) {
+    cat("   (none loaded)\n")
+  } else {
+    
+    # extract data properties
+    n_sentinel <- nrow(object$data)
+    n_positive <- sum(object$data$counts > 0)
+    n_obs <- sum(object$data$counts)
+    
+    cat(sprintf("   sentinel sites = %s (%s positive, %s negative)\n", n_sentinel, n_positive, n_sentinel - n_positive))
+    cat(sprintf("   total observations = %s\n", n_obs))
+  }
+  cat("\n")
+  
+  # print parameter sets summary
+  cat("PARAMETER SETS:\n")
+  if (length(object$parameter_sets) == 0) {
+    cat("   (none defined)\n")
+  } else {
+    
+    # print names of all sets
+    s <- object$active_set
+    for (i in 1:length(object$parameter_sets)) {
+      
+      # star next to active set
+      if (i == s) {
+        cat(" * ")
+      } else {
+        cat("   ")
+      }
+      
+      # print name of set
+      cat(sprintf("SET%s: %s\n", i, object$parameter_sets[[i]]$name))
+    }
+    cat("\n")
+    
+    # get details of active set
+    sentinel_radius <- object$parameter_sets[[s]]$sentinel_radius
+    min_lon <- object$parameter_sets[[s]]$min_lon
+    max_lon <- object$parameter_sets[[s]]$max_lon
+    min_lat <- object$parameter_sets[[s]]$min_lat
+    max_lat <- object$parameter_sets[[s]]$max_lat
+    sigma_model <- object$parameter_sets[[s]]$sigma_model
+    sigma_prior_mean <- object$parameter_sets[[s]]$sigma_prior_mean
+    sigma_prior_sd <- object$parameter_sets[[s]]$sigma_prior_sd
+    expected_popsize_prior_mean <- object$parameter_sets[[s]]$expected_popsize_prior_mean
+    expected_popsize_prior_sd <- object$parameter_sets[[s]]$expected_popsize_prior_sd
+    
+    # print details of active set
+    cat(sprintf("ACTIVE SET: SET%s\n", s))
+    cat(sprintf("   sentinel radius = %s\n", sentinel_radius))
+    cat(sprintf("   longitude range = [%s, %s]\n", min_lon, max_lon))
+    cat(sprintf("   latitude range = [%s, %s]\n", min_lat, max_lat))
+    cat(sprintf("   sigma:\n"))
+    cat(sprintf("      model = %s\n", sigma_model))
+    if (sigma_prior_sd == 0) {
+      cat(sprintf("      value = %s (exact prior)\n", sigma_prior_mean))
+    } else {
+      cat(sprintf("      prior mean = %s\n", sigma_prior_mean))
+      cat(sprintf("      prior SD = %s\n", sigma_prior_sd))
+    }
+    cat(sprintf("   expected population size:\n"))
+    if (expected_popsize_prior_sd == -1) {
+      cat(sprintf("      (improper prior)\n"))
+    } else if (expected_popsize_prior_sd == 0) {
+      cat(sprintf("      value = %s (exact prior)\n", expected_popsize_prior_mean))
+    } else {
+      cat(sprintf("      prior mean = %s\n", expected_popsize_prior_mean))
+      cat(sprintf("      prior SD = %s\n", expected_popsize_prior_sd))
+    }
+    
+  }
+  cat("\n")
   
 }
 
